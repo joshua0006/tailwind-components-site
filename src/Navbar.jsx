20c6fa5ce5adcbcf5a33c910de563f5bd0fa7c1f
joshua0006 <magnasejoshua@gmail.com>
2024-08-19 14:05:01 +0800

    try fix links

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -282,9 +282,9 @@ export default function Navbar() {
             <div className="space-y-6 border-t border-gray-200 px-4 py-6">
               {navigation.pages.map((page) => (
                 <div key={page.name} className="flow-root">
-                  <a href={page.href} className="-m-2 block p-2 font-medium text-gray-900">
+                  <Link to={page.href} className="-m-2 block p-2 font-medium text-gray-900">
                     {page.name}
-                  </a>
+                  </Link>
                 </div>
               ))}
             </div>
